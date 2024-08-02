98c4dadd576b15f1d928d2bbb06a100bf6aead1a
Sayar Basu <sayar.basu.cse26@heritageit.edu.in>
2024-08-02 21:51:37 +0530

    Create prog2.py

---
+++ Python-R/Assignment/prog2.py
@@ -0,0 +1,20 @@
+import math
+def isPrime(num):
+            for i in range (2,int (math.sqrt(num))+1):
+                       if ((num%i) == 0):
+                                    return " Not Prime "
+            return " Prime"
+def digitsum_even(num):
+            s=0
+            while(num>0):
+                        d=num%10
+                        s+=d
+                        num//=10
+            if ((s%2) == 0):
+                        return " Even Sum of Digits "
+            return " Not even Sum of Digits "
+for i in range (100,201):
+            p= isPrime(i)
+            e= digitsum_even(i)
+            print(i,p+e)
+       
