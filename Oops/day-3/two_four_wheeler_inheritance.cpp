fd1e20491cee7e6d601af8ed2c64ecdd17d4b8dd
Sayar Basu <sayar.basu.cse26@heritageit.edu.in>
2024-08-09 02:06:52 +0530

    Create two_four_wheeler_inheritance.cpp

---
+++ Oops/day-3/two_four_wheeler_inheritance.cpp
@@ -0,0 +1,59 @@
+#include <iostream>
+#include <string>
+using namespace std;
+class Vehicle
+{
+protected:
+    string name;
+    double mileage;
+protected:
+    void showdetails(string n, double m)
+    {
+        name=n,mileage=m;
+        cout<< "Name of the Vehicle = "<<name<<endl;
+        cout<< "Mileage it offers = "<<mileage<<endl;
+    }
+};
+class two_wheeler : public Vehicle
+{
+private:
+    string manufact;
+    int max_speed;
+public:
+    void inputdetails()
+    {
+        cout<<"FOR 2 Wheeler"<<endl<<endl;
+        cout<<"Enter manufacturer name "<<endl;
+        cin>>manufact;
+        cout<<"Enter Maximum Speed"<<endl;
+        cin>>max_speed;
+        cout<<"Manufacturer Name = "<<manufact<<endl;
+        cout<<"Maximum Speed = "<<max_speed<<endl<<endl;
+        Vehicle::showdetails("Bike",28.09);
+    }
+};
+class four_wheeler : public Vehicle
+{
+private:
+    string colour;
+    string type; //cedan or huchback
+public:
+    void inputdetails()
+    {
+        cout<<"FOR 4 Wheeler"<<endl<<endl;
+        cout<<"Enter colour of the car "<<endl;
+        cin>>colour;
+        cout<<"Enter Type "<<endl;
+        cin>>type;
+        cout<<"Colour of the car = "<<colour<<endl;
+        cout<<"Type = "<<type<<endl;
+        Vehicle::showdetails("Car",15.09); //user input
+    }
+};
+int main()
+{
+    two_wheeler obj1;
+    four_wheeler obj2;
+    obj1.inputdetails();
+    obj2.inputdetails();
+}
